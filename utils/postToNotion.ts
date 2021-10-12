dbe36f1db2a76e487f416270d1b4e126402d26f4
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-10-12 12:37:11 +0900

    add README

--- utils/postToNotion.ts
+++ utils/postToNotion.ts
@@ -3,9 +3,6 @@ import { Client } from '@notionhq/client';
 import { Block } from '@notionhq/client/build/src/api-types';
 import emojis from './emojis';
 
-//secret_hCSKm1ChCz2QpH1hEQ7TH01Rdh7ghrv6LTuBE5TY7x
-//8067fa92c8ac46a08483bceeb4a1020e
-
 export const postToNotion = async ({
     pageId,
     token,
